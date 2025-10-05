d0f649cc9976c99c91929633bd3b4fe50b1e146d
David Ibáñez <d.ibanez03@ufromail.cl>
2025-10-05 16:04:54 -0300

    Update UserRepository.java

--- src/main/java/com/gituhub/api_service/persistence/repository/UserRepository.java
+++ src/main/java/com/gituhub/api_service/persistence/repository/UserRepository.java
@@ -9,6 +9,6 @@ import java.util.Optional;
 @Repository
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
 
-    Optional<UserEntity> findUserByEmail(String email);
+    Optional<UserEntity> findUserByUsername(String username);
 
 }
