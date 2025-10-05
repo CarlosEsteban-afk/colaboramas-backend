f57b9bc865adecbcb0ce6fd5e67a67e470fdc767
David Ibáñez <d.ibanez03@ufromail.cl>
2025-10-05 17:35:53 -0300

    Create AuthResponse.java

---
+++ src/main/java/com/gituhub/api_service/controller/dto/AuthResponse.java
@@ -0,0 +1,6 @@
+package com.gituhub.api_service.controller.dto;
+
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+@JsonPropertyOrder({"username", "message", "jwt", "status"})
+public record AuthResponse(String username, String message, String jwt, boolean status) {}
