cc99bda6e44c745c2d60cd62faffa60d1c1c56f2
David Ibáñez <d.ibanez03@ufromail.cl>
2025-10-05 15:13:49 -0300

    Create PermissionEnum.java

---
+++ src/main/java/com/gituhub/api_service/persistence/entity/PermissionEnum.java
@@ -0,0 +1,15 @@
+package com.gituhub.api_service.persistence.entity;
+
+public enum PermissionEnum {
+    VER_RECOMENDACIONES,
+    BUSCAR_USUARIO,
+    CONTACTAR_USUARIO,
+    VER_EVENTOS,
+    AGREGAR_EVENTO,
+    EDITAR_EVENTO,
+    ELIMINAR_EVENTO,
+    VER_PERFIL,
+    EDITAR_PERFIL,
+    ELIMINAR_PERFIL,
+    VER_NOTIFICACIONES
+}
