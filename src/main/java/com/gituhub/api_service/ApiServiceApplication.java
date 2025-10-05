8c10af00f3ea0db041a950d6f4ae3bea785a1a8a
David Ibáñez <d.ibanez03@ufromail.cl>
2025-10-05 19:08:43 -0300

    Update ApiServiceApplication.java
    Se agregan las variables de entorno para JWT en main

--- src/main/java/com/gituhub/api_service/ApiServiceApplication.java
+++ src/main/java/com/gituhub/api_service/ApiServiceApplication.java
@@ -24,6 +24,8 @@ public class ApiServiceApplication {
 		System.setProperty("SPRING_JPA_HIBERNATE_DDL_AUTO", dotenv.get("SPRING_JPA_HIBERNATE_DDL_AUTO"));
 		System.setProperty("SPRING_SECURITY_USER_NAME", dotenv.get("SPRING_SECURITY_USER_NAME"));
 		System.setProperty("SPRING_SECURITY_USER_PASSWORD", dotenv.get("SPRING_SECURITY_USER_PASSWORD"));
+		System.setProperty("SECURITY.JWT.USER.GENERATOR", dotenv.get("SECURITY.JWT.USER.GENERATOR"));
+		System.setProperty("SECURITY.JWT.KEY.PRIVATE", dotenv.get("SECURITY.JWT.KEY.PRIVATE"));
 		SpringApplication.run(ApiServiceApplication.class, args);
 	}
 
