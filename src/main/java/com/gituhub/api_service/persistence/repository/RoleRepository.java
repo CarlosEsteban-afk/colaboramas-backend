7ed7560769c07f9fb43a90078b4abdd2edce6674
David Ibáñez <d.ibanez03@ufromail.cl>
2025-10-05 20:05:45 -0300

    Update RoleRepository.java

--- src/main/java/com/gituhub/api_service/persistence/repository/RoleRepository.java
+++ src/main/java/com/gituhub/api_service/persistence/repository/RoleRepository.java
@@ -6,9 +6,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface RoleRepository extends JpaRepository<RoleEntity, Long> {
 
     List<RoleEntity> findRolesByRoleNameIn(List<RoleEnum> roleNames);
+
+    Optional<RoleEntity> findByRoleName(RoleEnum roleName);
 }
