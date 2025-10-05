c8d4bf5f9b7e5e5078469acf401fc49283938b53
David Ibáñez <d.ibanez03@ufromail.cl>
2025-10-05 17:34:49 -0300

    Create AuthLoginRequest.java

---
+++ src/main/java/com/gituhub/api_service/controller/dto/AuthLoginRequest.java
@@ -0,0 +1,5 @@
+package com.gituhub.api_service.controller.dto;
+
+import jakarta.validation.constraints.NotBlank;
+
+public record AuthLoginRequest(@NotBlank String username,@NotBlank String password) {}
