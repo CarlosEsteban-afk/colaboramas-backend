479257d3588caa89adcf7bad46c3c90c215c8e91
OscarIgM <o.millalonco01@ufromail.cl>
2025-11-08 01:59:43 -0300

    change name from jwt to token

--- src/main/java/com/agora/auth/dto/AuthResponse.java
+++ src/main/java/com/agora/auth/dto/AuthResponse.java
@@ -2,5 +2,5 @@ package com.agora.auth.dto;
 
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 
-@JsonPropertyOrder({"email", "message", "jwt", "status"})
-public record AuthResponse(String email, String message, String jwt, boolean status) {}
+@JsonPropertyOrder({"email", "message", "token", "status"})
+public record AuthResponse(String email, String message, String token, boolean status) {}
