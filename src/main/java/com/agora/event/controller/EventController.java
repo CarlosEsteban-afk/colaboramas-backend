2cda05eac9cec53d6c9000e15c96d62c647f9386
Calegree <c.iturra07@ufromail.cl>
2025-11-26 15:36:03 -0300

    merge: develop into feature/events

--- src/main/java/com/agora/event/controller/EventController.java
+++ src/main/java/com/agora/event/controller/EventController.java
@@ -1,14 +1,5 @@
 package com.agora.event.controller;
 
-<<<<<<< HEAD
-import com.agora.event.dto.CreateEventRequest;
-import com.agora.event.dto.EventDTO;
-import com.agora.event.service.EventService;
-import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-=======
 import com.agora.event.model.Event;
 import com.agora.event.service.EventService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,31 +7,11 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
->>>>>>> develop
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/events")
-<<<<<<< HEAD
-@RequiredArgsConstructor
-public class EventController {
-
-    private final EventService eventService;
-
-    @PostMapping
-    public ResponseEntity<EventDTO> createEvent(@RequestBody CreateEventRequest request) {
-        EventDTO createdEvent = eventService.createEvent(request);
-        return new ResponseEntity<>(createdEvent, HttpStatus.CREATED);
-    }
-
-    @GetMapping
-    public ResponseEntity<List<EventDTO>> getAllEvents() {
-        List<EventDTO> events = eventService.getAllEvents();
-        return ResponseEntity.ok(events);
-    }
-}
-=======
+@RequestMapping("/events")
 public class EventController {
 
     @Autowired
@@ -51,4 +22,3 @@ public class EventController {
         return ResponseEntity.ok(eventService.getAllEvents());
     }
 }
->>>>>>> develop
